5e0527fe413e00c4896b17249e32ecdde80c4fd7
JithuMorrison <jithus2004@gmail.com>
2025-02-01 09:44:34 +0530

    modified header

--- script.js
+++ script.js
@@ -2,33 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
   let searchForm = document.querySelector('.header .search-form');
   document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
-      shoppingCart.classList.remove('active');
-      loginForm.classList.remove('active');
-      navbar.classList.remove('active');
   }
 
-
-  let loginForm = document.querySelector('.login-form');
-  document.querySelector('#login-btn').onclick = () => {
-    loginForm.classList.toggle('active');
-    searchForm.classList.remove('active');
-      shoppingCart.classList.remove('active');
-      navbar.classList.remove('active');
-  }
-  let navbar = document.querySelector('.navbar');
-  document.querySelector('#menu-btn').onclick = () => {
-    navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-      shoppingCart.classList.remove('active');
-      loginForm.classList.remove('active');
-  }
-  window.onscroll = () =>
-    {
-      searchForm.classList.remove('active');
-      shoppingCart.classList.remove('active');
-      loginForm.classList.remove('active');
-      navbar.classList.remove('active');
-    }
 var swiper = new Swiper(".products-slider",
       {
         loop:true, spaceBetween:20,
